433bb278a84640bc573d233893a7ba312e92e789
Sean Park <sean.inomire@gmail.com>
2020-12-04 17:54:38 -0800

    fetch movies

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,17 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 
-export default () => <h1>Home</h1>;
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+export default () => {
+  const { loading, error, data } = useQuery(GET_MOVIES);
+  console.log(loading, error, data);
+  return <h1>Home</h1>;
+};

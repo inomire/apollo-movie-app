313e073f271bc11a001cd81ba3ea0609941e134e
Sean Park <sean.inomire@gmail.com>
2020-12-06 23:42:02 -0800

    movie component

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,31 @@
-export default () => "Detail";
+import React from "react";
+import { useParams } from "react-router-dom";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      language
+      rating
+      description_intro
+    }
+  }
+`;
+
+export default () => {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: parseInt(id) },
+  });
+  console.log(loading, data);
+  if (loading) {
+    return "Loading...";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
+};
